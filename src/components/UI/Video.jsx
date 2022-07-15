24652e28058a4edefc11cc73aa7df3bfa8e11a9b
Binatik <sadxas999@mail.ru>
2022-07-15 18:25:38 +0300

    быстрый фикс iframe

--- src/components/UI/Video.jsx
+++ src/components/UI/Video.jsx
@@ -25,10 +25,8 @@ const Video = React.memo(({ path }) => {
             src={path}
             title="YouTube video player"
             frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture fullscreen"
-            allowFullScreen="true"
-            webkitallowfullscreen="true"
-            mozallowfullscreen="true"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true}
           ></Video.Video>
         </ThumbWrap>
       </VideoWrapper>
